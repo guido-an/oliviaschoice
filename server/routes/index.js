b354db8fbc482068a5aac0e855cfc941d96226f5
Guido <carucciguido@gmail.com>
2020-09-29 13:25:11 +0200

    cache api

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,22 @@ const router = express.Router()
 const axios = require('axios')
 const Product = require('../models/Product')
 
-const getProducts = async () => {
+let productsCache = []
+
+const cacheProductsInServer = async () => {
+  const products = await Product.find()
+  productsCache = products
+  console.log('CACHING')
+}
+
+cacheProductsInServer()
+
+const getProductsFromAPI = async () => {
   try {
+    await Product.deleteMany()
     const response = await axios.get(process.env.API_URL)
-    const products = response.data
-    products.forEach(async product => {
+    const productsFromAPI = response.data // Get products from API
+    productsFromAPI.forEach(async product => { // Create products in database
       const name = product.MG87_DESCART
       await Product.create({ name })
     })
@@ -16,17 +27,17 @@ const getProducts = async () => {
   }
 }
 
-setInterval(function () {
+setInterval(async () => {
   var date = new Date() // Create a Date object to find out what time it is
   if (date.getHours() === 8 && date.getMinutes() === 0) { // Check the time
-    getProducts() // Update database
+    await getProductsFromAPI()
+    await cacheProductsInServer()
   }
 }, 60000) // Check each minute (to be changed)
 
 router.get('/api/products', async (req, res) => {
   try {
-    const products = await Product.find()
-    res.status(200).send(products)
+    res.status(200).send(productsCache)
   } catch (err) {
     res.status(500).send('Something went wrong on this call: /api/products')
   }
