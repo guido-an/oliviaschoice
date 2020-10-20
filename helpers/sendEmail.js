edbaa48e094c15d4416e4081eedaea6cdaf28e02
Guido <carucciguido@gmail.com>
2020-10-20 12:27:52 +0100

    send email

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,27 +1,48 @@
+// const nodemailer = require('nodemailer');
+
+// function sendEmail(fromEmail, toEmail, title, message) {
+//   let transporter = nodemailer.createTransport({
+//     host: 'hostingssd12.netsons.net',
+//     port: 465,
+//     secure: true, // use TLS
+//     auth: {
+//       user: process.env.NODEMAILER_EMAIL,
+//       pass: process.env.NODEMAILER_PSW,
+//     },
+//   });
+
+//   transporter.sendMail({
+//     // email to the USER
+//     from: fromEmail,
+//     to: toEmail,
+//     subject: title,
+//     text: '',
+//     html: message,
+//   });
+// }
+
+// module.exports = sendEmail
+
 const nodemailer = require('nodemailer');
 
 function sendEmail(fromEmail, toEmail, title, message) {
+    console.log('SENDEMAIL')
   let transporter = nodemailer.createTransport({
-    host: 'hostingssd12.netsons.net',
-    port: 465,
+    service: 'Gmail',
     secure: true, // use TLS
-    // auth: {
-    //   user: process.env.NODEMAILER_EMAIL,
-    //   pass: process.env.NODEMAILER_PSW,
-    // },
     auth: {
-        user: process.env.NODEMAILER_EMAIL,
-        pass: process.env.NODEMAILER_PSW,
-      },
+      user: 'federico.barriola@gmail.com',
+      pass: 'tumadreen.4',
+    },
   });
 
   transporter.sendMail({
     // email to the USER
-    from: fromEmail,
-    to: toEmail,
-    subject: title,
-    text: '',
-    html: message,
+    from: 'federico.barriola@gmail.com',
+    to: 'carucciguido@gmail.com',
+    subject: 'test email',
+    text: 'This is the text',
+    html: 'this is the message field',
   });
 }
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const Order = require('../models/Order')
 const User = require('../models/User')
+const sendEmail = require('../helpers/sendEmail')
+
 
 router.post('/create-order', async (req, res) => {
   const { _id, shippingInfo, totalPrice, paid, productsInCart } = req.body
@@ -35,6 +37,7 @@ router.post('/create-order', async (req, res) => {
 router.post('/update-order', async (req, res) => {
   try {
     await Order.findByIdAndUpdate({ _id: req.body._id }, { paid: true })
+    sendEmail()
     res.status(200).json({ message: 'Order updated to paid' })
   } catch (err) {
     console.error(err)
