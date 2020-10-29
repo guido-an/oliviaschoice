1d6b71428c309ea194d21b30ec61d5cc36699fea
Guido <carucciguido@gmail.com>
2020-10-29 11:45:56 +0000

    shipping pro connection

--- client/pages/user/ordini.js
+++ client/pages/user/ordini.js
@@ -2,35 +2,33 @@ import React, { useEffect, useContext, useState } from 'react'
 import { UserContext } from '../../contexts/UserContext'
 import Link from 'next/link'
 
-
 const Ordini = () => {
-const { getOrders } = useContext(UserContext)
-const [orders, setOrders] = useState([])
+  const { getOrders } = useContext(UserContext)
+  const [orders, setOrders] = useState([])
 
-
-useEffect(() => {
-    async function getOrdersFromDB(){
-       const ordersFromDB = await getOrders()
-       setOrders(ordersFromDB)
+  useEffect(() => {
+    async function getOrdersFromDB () {
+      const ordersFromDB = await getOrders()
+      setOrders(ordersFromDB)
     }
     getOrdersFromDB()
-}, [])
+  }, [])
 
-   return(
-       <div>
-           <h1>Ordini</h1>
-           {orders.map(order => {
-               return <div key={order._id}>
-                   <p><strong>ORDER</strong></p>
-                   <p>{order._id}</p>
-                   <p>{order.created_at}</p>
-                   <Link href='/user/ordine/[id]' as={`/user/ordine/${encodeURIComponent(order._id)}`}>
+  return (
+    <div>
+      <h1>Ordini</h1>
+      {orders.map(order => {
+        return <div key={order._id}>
+          <p><strong>ORDER</strong></p>
+          <p>{order._id}</p>
+          <p>{order.created_at}</p>
+          <Link href='/user/ordine/[id]' as={`/user/ordine/${encodeURIComponent(order._id)}`}>
                       View order
-                   </Link>
-               </div>
-           })}
-       </div>
-   )
+          </Link>
+        </div>
+      })}
+    </div>
+  )
 }
 
 export default Ordini
