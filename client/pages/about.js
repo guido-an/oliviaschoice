c324559def196848ee4fdc10b3c5eb6fcc48e72e
casikami <fede@barriola.com.uy>
2020-10-29 12:03:48 +0000

    next buids

--- client/pages/about.js
+++
@@ -1,9 +0,0 @@
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <span class="hs-cta-wrapper" id="hs-cta-wrapper-3064d71e-3bfd-4b67-b471-1544ee790d2d"><span class="hs-cta-node hs-cta-3064d71e-3bfd-4b67-b471-1544ee790d2d" id="hs-cta-3064d71e-3bfd-4b67-b471-1544ee790d2d"><div id="hs-cta-ie-element"></div><a href="https://cta-redirect.hubspot.com/cta/redirect/8068265/3064d71e-3bfd-4b67-b471-1544ee790d2d" ><img class="hs-cta-img" id="hs-cta-img-3064d71e-3bfd-4b67-b471-1544ee790d2d" style="border-width:0px;" src="https://no-cache.hubspot.com/cta/default/8068265/3064d71e-3bfd-4b67-b471-1544ee790d2d.png"  alt="New call-to-action"/></a></span><script charset="utf-8" src="https://js.hscta.net/cta/current.js"></script><script type="text/javascript"> hbspt.cta.load(8068265, '3064d71e-3bfd-4b67-b471-1544ee790d2d', {}); </script></span>
-    </div>
-  )
-}
-export default About
