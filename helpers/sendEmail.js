db3c7850920913c855da6018421225303e68cd52
Guido <carucciguido@gmail.com>
2021-01-07 13:08:57 +0000

    categories total products

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,49 +1,50 @@
-// const nodemailer = require('nodemailer');
-
-// function sendEmail(fromEmail, toEmail, title, message) {
-//   let transporter = nodemailer.createTransport({
-//     host: 'hostingssd12.netsons.net',
-//     port: 465,
-//     secure: true, // use TLS
-//     auth: {
-//       user: process.env.NODEMAILER_EMAIL,
-//       pass: process.env.NODEMAILER_PSW,
-//     },
-//   });
-
-//   transporter.sendMail({
-//     // email to the USER
-//     from: fromEmail,
-//     to: toEmail,
-//     subject: title,
-//     text: '',
-//     html: message,
-//   });
-// }
-
-// module.exports = sendEmail
-
 const nodemailer = require('nodemailer')
 
-function sendEmail (toEmail, title, message) {
-  console.log('SENDEMAIL')
+function sendEmail () {
+  console.log('SENDING EMAIL')
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    host: 'hostingssd12.netsons.net',
+    port: 465,
     secure: true, // use TLS
     auth: {
-      user: 'service email',
-      pass: '*****'
+      user: process.env.NODEMAILER_EMAIL,
+      pass: process.env.NODEMAILER_PSW
     }
   })
 
   transporter.sendMail({
     // email to the USER
-    from: '',
-    to: '',
-    subject: 'test email',
-    text: 'This is the text',
-    html: 'this is the message field'
+    from: process.env.NODEMAILER_EMAIL,
+    to: 'carucciguido@gmail.com',
+    subject: 'new email',
+    text: '',
+    html: 'message'
   })
 }
 
 module.exports = sendEmail
+
+// const nodemailer = require('nodemailer')
+
+// function sendEmail (toEmail, title, message) {
+//   console.log('SENDEMAIL')
+//   const transporter = nodemailer.createTransport({
+//     service: 'Gmail',
+//     secure: true, // use TLS
+//     auth: {
+//       user: 'service email',
+//       pass: '*****'
+//     }
+//   })
+
+//   transporter.sendMail({
+//     // email to the USER
+//     from: '',
+//     to: '',
+//     subject: 'test email',
+//     text: 'This is the text',
+//     html: 'this is the message field'
+//   })
+// }
+
+// module.exports = sendEmail
--- routes/stripe.js
+++ routes/stripe.js
@@ -6,14 +6,19 @@ const Product = require('../models/Product')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const calculateOrderAmount = async items => {
+  console.log(items.length, 'items')
   let totalPrice = 0
   try {
     // with forEach doesn't work
     for (let i = 0; i < items.length; i++) {
       const product = await Product.findById({ _id: items[i]._id })
+      console.log(product, 'product')
       const productPrice = product.price * items[i].boughtQuantity
+      console.log(productPrice, 'productPrice')
+      console.log(items[i].boughtQuantity, 'product boughtQuantity')
       totalPrice = totalPrice + productPrice
     }
+    console.log(totalPrice * 100, 'totale price')
     return totalPrice * 100
   } catch (e) {
     console.log(e)
@@ -22,7 +27,6 @@ const calculateOrderAmount = async items => {
 
 router.post('/create-payment-intent', async (req, res) => {
   const { items } = req.body
-
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: await calculateOrderAmount(items),
