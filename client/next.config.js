3c0d7f0d04be20efcc54d2ce230a7e41c7580d0b
Guido <carucciguido@gmail.com>
2021-01-07 10:50:44 +0000

    deploy fix

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
+
 const withImages = require('next-images')
 
 module.exports = withImages({
   env: {
     APP_API: process.env.APP_API,
     STRIPE_PK: process.env.STRIPE_PK
-  }
-
+  },
+  target: 'experimental-serverless-trace'
 })
