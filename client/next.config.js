ddae1f971aa5b14114452fca5d79914736478f88
Tofiq Quadri <tofiqquadri@ymail.com>
2021-02-18 22:19:15 +0530

    Fixing CORS Issue [Server], Environment issues [Client], Login refresh issue in production

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
+require('dotenv').config({ path: `./.env.${process.env.ENVIRONMENT}` });
 
 const withImages = require('next-images')
 
 module.exports = withImages({
   env: {
-    APP_API: 'http://localhost:5000',
+    APP_API: process.env.APP_API,
     STRIPE_PK: process.env.STRIPE_PK
   },
   target: 'serverless'
